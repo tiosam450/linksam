4a5ed08c4a34e91986cc3e7365b26fbe511c64dc
Samuel <tiosam450@hotmail.com>
2024-08-05 13:01:08 -0400

    Desk MV

--- src/paginas/home/index.tsx
+++ src/paginas/home/index.tsx
@@ -1,9 +1,13 @@
+import fotoPerfil from '../../assets/img/foto_perfil.jpg'
 
 export default function Home(){
 
     return(
-        <>
-        <h1 className="">Página Home</h1>
-        </>
+        <section className=' w-100 h-screen flex justify-center py-48'>
+        <div>
+        <img className='rounded-full w-32 h-32;
+        ' src={fotoPerfil} alt="" />
+        </div>
+        </section>
     )
 }
